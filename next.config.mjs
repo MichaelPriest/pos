6a0f8e4b9c855bf4fbe04a563fc68b72409f2909
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-08-19 16:25:29 -0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  async redirects() {
-    return [{ source: '/', destination: '/loja', permanent: false }];
-  },
-};
-
-export default nextConfig;
